17c573433fcb4ae623d6d30a5b5116f4d96233de
Ryan Wang <deanmax@gmail.com>
2013-12-01 22:40:17 -0600

    To implement flock

--- test4.sh
+++ test4.sh
@@ -1,10 +1,10 @@
 rm -f lan?  hout?  hin?
 rm -f rout?
-host 0 0 sender 50 20&
-host 1 1 receiver &
-host 2 2 receiver &
-router 0 0 1 &
-router 1 1 2 &
-router 2 2 3 &
-router 3 3 0 &
-controller host 0 1 2 router 0 1 2 3 lan 0 1 2 3&
+./host 0 0 sender 50 20&
+./host 1 1 receiver &
+./host 2 2 receiver &
+./router 0 0 1 &
+./router 1 1 2 &
+./router 2 2 3 &
+./router 3 3 0 &
+./controller host 0 1 2 router 0 1 2 3 lan 0 1 2 3&
--- test6.sh
+++ test6.sh
@@ -1,6 +1,6 @@
 rm -f lan?  hout?  hin?
 rm -f rout?
-host 0 0 sender 20 20&
-router 0 0 1 &
-router 1 1 2 &
-controller host 0 1 router 0 1 lan 0 1 2&
+./host 0 0 sender 20 20&
+./router 0 0 1 &
+./router 1 1 2 &
+./controller host 0 1 router 0 1 lan 0 1 2&
